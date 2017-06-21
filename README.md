c26450d298f7d18d8516334cdf22fd2dfb6d6f33
ericjef <richard.jeffery@ericsson.com>
2017-06-21 07:09:40 +0100

    Adding Documentation

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # psychic-octo-parakeet
+
+This application implements a queue populated with Request objects. Objects can be added and removed. Other operations are also available
+
+## Compatibility
+ JDK 8
+ 
+## Build and Deploy
+### Build
+To build the jar simply run `./gradlew build` in the base directory of the repository. This will generate a in the build/libs directory called psychic-octo-parakeet-{version}.jar
+
+## Deploy
+For simplicity this jar provides it's own tomcat container. To run the application use java to run the jar: `java -jar build/libs/psychic-octo-parakeet-{version}.jar`
+
+## REST service
+Base application URL: http://localhost:8080/queue-app
+
+| Resource        | Method           | Parameters  | Request Body   |
+| ------------- |:-------------:|:-----:| -------------:| 
+| requests/enqueue      | PUT |   | {"customerId": 5, "date": "2017-06-19 09:00:00"} |
+| requests/delete/{id}      | DELETE |       |       |
+| requests/dequeue      | GET     |      |      |
+| requests             | GET   |      |       |
+| requests/index/{id}  | GET  |       |      |
+| requests/meanwait    | GET  | date  |      |
--- src/main/java/home/work/queue/controllers/RequestController.java
+++ src/main/java/home/work/queue/controllers/RequestController.java
@@ -2,8 +2,6 @@ package home.work.queue.controllers;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -12,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,108 +22,140 @@ import org.springframework.web.util.UriComponentsBuilder;
 import home.work.queue.models.*;
 
 /**
- * Controller class for testing user's repositories classes.
+ * Controller class providing REST access Request objects.
  *
- * @author netgloo
+ * @author Richard
  */
 @RestController
 public class RequestController {
 
-  // ==============
-  // PRIVATE FIELDS
-  // ==============
+	// ==============
+	// PRIVATE FIELDS
+	// ==============
 
-  @Autowired
-  private RequestRepository requestRepository;
+	@Autowired
+	private RequestRepository requestRepository;
 
+	// ==============
+	// PUBLIC METHODS
+	// ==============
 
-  // ==============
-  // PUBLIC METHODS
-  // ==============
-  
-  /**
-   * /user/create/person?email=[email]&firstName=[firstName] -> create a new 
-   * person user and save it in the database.
-   * 
-   * @param email The person's email
-   * @param firstName The person's first name
-   * @return a string describing if the person is succesfully created or not.
-   */
-  @RequestMapping(value="requests/enqueue", method=RequestMethod.PUT)
-  @ResponseBody
-  public ResponseEntity<Void> enqueueRequest(@RequestBody Request request, UriComponentsBuilder ucBuilder) {
+	/**
+	 * /requests/enqueue -> add a new request to the queue
+	 * 
+	 * @body { "customerId": 3, "date": "2017-06-19 09:00:00" }
+	 * 
+	 * @param customerId
+	 *            The id of the Request to be enqueued
+	 * @param date
+	 *            The date that the Request is added in format "yyyy-MM-dd
+	 *            hh:mm:ss"
+	 * @return No body is returned. Will return status code 201 or 409.
+	 */
+	@RequestMapping(value = "requests/enqueue", method = RequestMethod.PUT)
+	@ResponseBody
+	public ResponseEntity<Void> enqueueRequest(@RequestBody Request request, UriComponentsBuilder ucBuilder) {
 
-   	if(requestRepository.exists(request.getCustomerId())){
-        System.out.println("A Request with id "+request.getCustomerId()+" already exist");
-        return new ResponseEntity<Void>(HttpStatus.CONFLICT);
-    }
-   	requestRepository.save(request);
-        
-    HttpHeaders headers = new HttpHeaders();
-    headers.setLocation(ucBuilder.path("/requests/{id}").buildAndExpand(request.getCustomerId()).toUri());
-    return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
-  }
-  
-  @RequestMapping(value="requests/dequeue", method=RequestMethod.GET)
-  @ResponseBody
-  public ResponseEntity<String> dequeueRequest(UriComponentsBuilder ucBuilder) {
+		if (requestRepository.exists(request.getCustomerId())) {
+			System.out.println("A Request with id " + request.getCustomerId() + " already exist");
+			return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+		}
+		requestRepository.save(request);
 
-   	List<Request> requests = requestRepository.findAll();
-   	Collections.sort(requests);
-   	Request request = requests.get(0);
-   	String requestJson = "{\"customerId\": "+request.getCustomerId().toString()+",\"date\": \""+request.getDate().toString()+"\"}";
-    requestRepository.delete(request);
-    return new ResponseEntity<String>(requestJson, HttpStatus.OK);
-  }
-  
-  /**
-   * /user/get?email=[email] -> return the user having the passed email.
-   * 
-   * @param email The email to search in the database.
-   * @return The user id or a message error if the user is not found.
-   */
-  @RequestMapping(value = "/requests", method=RequestMethod.GET)
-  @ResponseBody
-  public ResponseEntity<List<Request>> getSortedRequests() {
-    List<Request> requests = requestRepository.findAll();
-    Collections.sort(requests);
-    return new ResponseEntity<List<Request>>(requests, HttpStatus.OK);
-  }
-  
-  @RequestMapping(value="requests/index/{id}", method=RequestMethod.GET)
-  @ResponseBody
-  public ResponseEntity<String> getIndexOfRequest(@PathVariable("id") Long customerId) {
-	  List<Request> requests = requestRepository.findAll();
-	  Collections.sort(requests);
-	  Request request = requestRepository.findByCustomerId(customerId);
-	  int index = requests.indexOf(request);
-	  if (index == -1) {
-		  return new ResponseEntity<String>("{}", HttpStatus.NOT_FOUND);
-	  }
-	  return new ResponseEntity<String>("{\"index\": "+index+"}", HttpStatus.OK);
-  }
-  
-  @RequestMapping(value="requests/meanwait", method=RequestMethod.GET)
-  @ResponseBody
-  public ResponseEntity<String> getMeanWaitTimeInQueue(@RequestParam(value="date") String dateStr) {
-	  
-	  SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-	  Date date;
-	  try {
-		  date = format.parse( dateStr );
-	  } catch (ParseException e) {
-		  return new ResponseEntity<String>("{}", HttpStatus.NOT_FOUND);
-	  }
-	  List<Request> requests = requestRepository.findAll();
-	  if(requests.size() == 0){
-		  return new ResponseEntity<String>("{}", HttpStatus.NOT_FOUND);
-	  }
-	  Long totalTimeInQueue = 0L;
-	  for (Request request : requests) {
-		  totalTimeInQueue += (date.getTime() - request.getDate().getTime()) /1000;
-	  }
-	  long meanTimeInQueue = totalTimeInQueue/requests.size();
-	  return new ResponseEntity<String>("{\"queueWaitTime\": "+meanTimeInQueue+"}", HttpStatus.OK);
-  }
-  
-} // class UserController
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(ucBuilder.path("/requests/{id}").buildAndExpand(request.getCustomerId()).toUri());
+		return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+	}
+
+	/**
+	 * /requests/dequeue -> Retrieve highest priority request from queue
+	 * and delete from database
+	 * 
+	 * @return No body is returned. Will return status code 200 or 404.
+	 */
+	@RequestMapping(value = "requests/dequeue", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<String> dequeueRequest(UriComponentsBuilder ucBuilder) {
+
+		List<Request> requests = requestRepository.findAll();
+		if (requests.size() == 0) {
+			return new ResponseEntity<String>("{}", HttpStatus.NOT_FOUND);
+		}
+		Collections.sort(requests);
+		Request request = requests.get(0);
+		String requestJson = "{\"customerId\": " + request.getCustomerId().toString() + ",\"date\": \""
+				+ request.getDate().toString() + "\"}";
+		requestRepository.delete(request);
+		return new ResponseEntity<String>(requestJson, HttpStatus.OK);
+	}
+
+	/**
+	 * /requests -> returns the contents of the queue sorted by
+	 * priority (Highest -> Lowest)
+	 * 
+	 * @return Returns sorted contents of queue. 
+	 * 			Will return status code 200.
+	 */
+	@RequestMapping(value = "/requests", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<List<Request>> getSortedRequests() {
+		List<Request> requests = requestRepository.findAll();
+		Collections.sort(requests);
+		return new ResponseEntity<List<Request>>(requests, HttpStatus.OK);
+	}
+
+	/**
+	 * /requests/index/{id} -> Return position in queue of Request with 
+	 * 							customerId of {id}
+	 * 
+	 * @param customerId
+	 *            The id of the Request
+	 * @return Returns json with structure: "{"index": "2"}". 
+	 * 			Will return status code 200 or 404.
+	 */
+	@RequestMapping(value = "requests/index/{id}", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<String> getIndexOfRequest(@PathVariable("id") Long customerId) {
+		List<Request> requests = requestRepository.findAll();
+		Collections.sort(requests);
+		Request request = requestRepository.findByCustomerId(customerId);
+		int index = requests.indexOf(request);
+		if (index == -1) {
+			return new ResponseEntity<String>("{}", HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<String>("{\"index\": " + index + "}", HttpStatus.OK);
+	}
+
+	/**
+	 * /requests/meanwait -> get mean time that Requests have been in 
+	 * 						the queue in seconds
+	 * 
+	 * @param date
+	 *            The date in format "yyyy-MM-dd hh:mm:ss"
+	 * @return Returns json with structure: "{"queueWaitTime": "30000"}". 
+	 * 			Will return status code 200 or 404.
+	 */
+	@RequestMapping(value = "requests/meanwait", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<String> getMeanWaitTimeInQueue(@RequestParam(value = "date") String dateStr) {
+
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		Date date;
+		try {
+			date = format.parse(dateStr);
+		} catch (ParseException e) {
+			return new ResponseEntity<String>("{}", HttpStatus.NOT_FOUND);
+		}
+		List<Request> requests = requestRepository.findAll();
+		if (requests.size() == 0) {
+			return new ResponseEntity<String>("{}", HttpStatus.NOT_FOUND);
+		}
+		Long totalTimeInQueue = 0L;
+		for (Request request : requests) {
+			totalTimeInQueue += (date.getTime() - request.getDate().getTime()) / 1000;
+		}
+		long meanTimeInQueue = totalTimeInQueue / requests.size();
+		return new ResponseEntity<String>("{\"queueWaitTime\": " + meanTimeInQueue + "}", HttpStatus.OK);
+	}
+
+}
